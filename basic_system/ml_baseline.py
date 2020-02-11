32fa5013eb7675806260e3f752a65b67db642243
laurencgreen <44449955+laurencgreen@users.noreply.github.com>
2020-02-11 08:43:31 +0100

    update

--- basic_system/ml_baseline.py
+++ basic_system/ml_baseline.py
@@ -4,12 +4,10 @@ from sklearn import svm
 import pickle
 from utils import create_new_folder, get_column_list, concat_dataframes, split_intensity_labels, read_subsets
 from sklearn.metrics import classification_report
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
-
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
 import sys
 sys.path.insert(0, "feature_extraction")
@@ -17,24 +15,49 @@ from w2v import open_embeddings_model, embed_sentences
 from lex2v import lex_main
 # from s2v import dl_sent_matrix
 
-def train_model(X_train, y_train, X_test, y_test, model_name):
+def train_model(train_df, test_df, model_name, EMBEDDINGS, LEXICON):
     """Input X_train, y_train, X_test, y_test and classification model. Print classification metrics -> see
     get_metrics() function"""
     print("Training model")
 
-    if model_name == "svm":
-        model = svm.LinearSVC(max_iter=10000, dual=False, C=0.1)
+    model = define_model(model_name)
 
-    clf = Pipeline([('vect', CountVectorizer()),
-                    ('frm', TfidfTransformer()),
-                ('clf', model)])
-                
+    if EMBEDDINGS == "False" and LEXICON == "False":
+        X_train, y_train, X_test, y_test = encode_bow_data(train_df, test_df)
+        clf = bow_pipeline(model)
+
+    else:
+        y_train = train_df.intensity_scores.values[:20]
+        y_test = test_df.intensity_scores.values[:8]
+        train_features_df, test_features_df = get_features(train_df, test_df, EMBEDDINGS, LEXICON)
+        X_train = train_features_df.to_dict(orient="records")
+        print(X_train)
+        X_test = test_features_df.to_dict(orient="records")
+        clf = dict_pipeline(model)
+    
     clf.fit(X_train, y_train)
     save_classification_model(model_name, clf)
+    return X_test, y_test
 
+def define_model(model_name):
+    if model_name == "svm":
+        model = svm.LinearSVC(max_iter=10000, dual=False, C=0.1)
+    return model
+
+def encode_bow_data(train_df, test_df):
+    X_train = train_df.tweets.values
+    y_train = train_df.intensity_scores.values
+
+    X_test = test_df.tweets.values
+    y_test = test_df.intensity_scores.values
+    return X_train, y_train, X_test, y_test
 
 def bow_pipeline(model):
     clf = Pipeline([('vect', CountVectorizer()), ('frm', TfidfTransformer()), ('clf', model)])
+    return clf
+
+def dict_pipeline(model):
+    clf = Pipeline([('vect', DictVectorizer()), ('scaler', StandardScaler(with_mean=False)), ('clf', model)])
     return clf
 
 def save_classification_model(filename, model):
@@ -47,39 +70,21 @@ def save_classification_model(filename, model):
     print(f"Saved {filename} model in {cm_folder_path}")
 
 
-def fit_transform_vec(train_features_df, validation_features_df, test_features_df): 
-
-    vec = TfidfVectorizer()
-    
-    X_test = test_features_df.tweets.values
-    X_train = train_features_df.tweets.values.tolist()
-
-    X_test = vec.transform(X_test).toarray()
-
-    return X_train, X_val, X_test
-
-
-def get_labels(train_df, validation_df, test_df, column_header):
-    """Input dataframe and column_header. Return as a list of labels ready for model setup."""
-    y_train = train_df[column_header].values.tolist() 
-    y_val = validation_df[column_header].values.tolist()
-    y_test = test_df[column_header].values.tolist()     
-    return y_train, y_val, y_test
-
-
 def prepare_data(df, EMBEDDINGS):
-    # if "s_" in EMBEDDINGS:
-    tweet_list = df["tweets"].values.tolist() 
-    # else:
-        # tweet_list = [tweet.split() for tweet in df["tweets"].tolist()]  # list of list of tokens
-        # df = pd.DataFrame({0: tweet_list})
-        # df.columns= [["tweets"]]
+    if "s_" in EMBEDDINGS:
+        tweet_list = df["tweets"].values.tolist() 
+
+    else:
+        tweet_list = [tweet.split() for tweet in df["tweets"].tolist()]  # list of list of tokens
+        df = pd.DataFrame({0: tweet_list})
+        df.columns= [["tweets"]]
     return tweet_list, df
 
 
-def get_features(train_df, validation_df, test_df, EMBEDDINGS, LEXICON):
+def get_features(train_df, test_df, EMBEDDINGS, LEXICON):
+    train_df = train_df.iloc[:20]
+    test_df = test_df.iloc[:8]
     train_tweet_list, train_df = prepare_data(train_df, EMBEDDINGS)
-    validation_tweet_list, validation_df = prepare_data(validation_df, EMBEDDINGS)
     test_tweet_list, test_df = prepare_data(test_df, EMBEDDINGS)
 
     if EMBEDDINGS != "False":
@@ -88,32 +93,28 @@ def get_features(train_df, validation_df, test_df, EMBEDDINGS, LEXICON):
         elif "s_sentT" in EMBEDDINGS:
             embedding_model = open_model(EMBEDDINGS)
             train_df = sentence_transformer(train_tweet_list, embedding_model)
-            validation_df = sentence_transformer(validation_tweet_list, embedding_model)
             test_df = sentence_transformer(test_tweet_list, embedding_model)
         else:
             embeddings_index = open_embeddings_model(EMBEDDINGS)
             train_df = embed_sentences(train_df, "tweets", embeddings_index, EMBEDDINGS)
-            validation_df = embed_sentences(validation_df, "tweets", embeddings_index, EMBEDDINGS)
+
             test_df = embed_sentences(test_df, "tweets", embeddings_index, EMBEDDINGS)
     
     if LEXICON != "False":
         train_lex_list = lex_main(train_tweet_list, LEXICON)
-        validation_lex_list = lex_main(validation_tweet_list, LEXICON)
         test_lex_list = lex_main(test_tweet_list, LEXICON)
         train_df["lex"] = train_lex_list
-        validation_df["lex"] = validation_lex_list
         test_df["lex"] = test_lex_list
 
     train_df = train_df.astype(str)
-    validation_df = validation_df.astype(str)
     test_df = test_df.astype(str)
-    return train_df, validation_df, test_df
+    return train_df, test_df
 
 
 def encode_data(train_df, validation_df, test_df, EMBEDDINGS, LEXICON):
-    y_train, y_val, y_test = get_labels(train_df, validation_df, test_df, "intensity_scores")
-    train_features_df, validation_features_df, test_features_df = get_features(train_df, validation_df, test_df, EMBEDDINGS, LEXICON)
-    X_train, X_val, X_test = fit_transform_vec(train_features_df, validation_features_df, test_features_df)
+    y_train, y_test = get_labels(train_df, test_df, "intensity_scores")
+    train_features_df, test_features_df = get_features(train_df, test_df, EMBEDDINGS, LEXICON)
+    # X_train, X_val, X_test = fit_transform_vec(train_features_df, validation_features_df, test_features_df)
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
@@ -132,16 +133,10 @@ def ml_main(train_filepath, validation_filepath, test_filepath, model_name, PREP
 
     train_df = pd.concat([train_df, validation_df], axis=0)
 
-    X_train = train_df.tweets.values
-    y_train = train_df.intensity_scores.values
-
-    X_test = test_df.tweets.values
-    y_test = test_df.intensity_scores.values
-
     # X_train, y_train, X_val, y_val, X_test, y_test = encode_data(train_df, validation_df, test_df, EMBEDDINGS, LEXICON)
     # print(X_train.shape, X_val.shape, X_test.shape)
 
-    train_model(X_train, y_train, X_test, y_test, model_name)
+    X_test, y_test = train_model(train_df, test_df, model_name, EMBEDDINGS, LEXICON)
 
     print("Finished training and testing")
     return X_test, y_test
